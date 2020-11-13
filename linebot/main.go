6d94fa5e22533c451472fe330af258ef54353566
Satoshi Ota <sota.developer@gmail.com>
2020-11-13 12:42:58 +0900

    Add webhook events

--- linebot/main.go
+++ linebot/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,9 +12,14 @@ import (
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
+type Webhook struct {
+	Destination string           `json:"destination"`
+	Events      []*linebot.Event `json:"events"`
+}
+
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
-	_, err := linebot.New(
+	bot, err := linebot.New(
 		os.Getenv("LINE_CHANNEL_SECRET"),
 		os.Getenv("LINE_CHANNEL_ACCESS_TOKEN"),
 	)
@@ -27,6 +33,32 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 	log.Print(request.Headers)
 	log.Print(request.Body)
+
+	var webhook Webhook
+
+	if err := json.Unmarshal([]byte(request.Body), &webhook); err != nil {
+		log.Print(err)
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusBadRequest,
+			Body:       fmt.Sprintf(`{"message":"%s"}`+"\n", http.StatusText(http.StatusBadRequest)),
+		}, nil
+	}
+
+	for _, event := range webhook.Events {
+		switch event.Type {
+		case linebot.EventTypeMessage:
+			switch m := event.Message.(type) {
+			case *linebot.TextMessage:
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(m.Text)).Do(); err != nil {
+					log.Print(err)
+					return events.APIGatewayProxyResponse{
+						StatusCode: http.StatusInternalServerError,
+						Body:       fmt.Sprintf(`{"message":"%s"}`+"\n", http.StatusText(http.StatusBadRequest)),
+					}, nil
+				}
+			}
+		}
+	}
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
